d7a326048752c042d082fa7fb4dee122434dbaf9
jyotiradityz <patiljyotiraditya123@gmail.com>
2023-08-08 18:12:28 +0530

    Learned Basic Modules

---
+++ Basic Modules/4-fs-sync.js
@@ -0,0 +1,7 @@
+const {readFileSync,writeFileSync}=require('fs') 
+const fisrt=readFileSync('../content/first.txt','utf-8')
+const second=readFileSync('../content/second.txt','utf-8')
+console.log(fisrt);
+console.log(second);
+
+writeFileSync('../content/res.txt', `heheheeh adding ${fisrt}`)
