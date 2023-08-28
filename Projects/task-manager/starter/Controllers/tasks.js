86ea34bcd4dac30030707dd0f3195a0c5abe4ceb
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2023-08-28 11:38:23 +0530

    "Added mongo Controllers"

--- Projects/task-manager/starter/Controllers/tasks.js
+++ Projects/task-manager/starter/Controllers/tasks.js
@@ -1,19 +1,61 @@
-const express=require('express')
-
-const getAlltask=(req,res)=>{
-    res.send('Get Your All Tasks')
+const Task=require('../models/Task')
+const getAlltask=async (req,res)=>{
+    try{
+        const tasks=await Task.find({})
+        res.status(201).json({tasks})
+    }
+    catch(err){
+        res.status(500).json({msg:err})
+    }
 } 
-const createTask=(req,res)=>{
-    res.json(req.body)
+const createTask= async (req,res)=>{
+    try{
+        const task= await Task.create(req.body)
+        res.status(201).json({task})
+    }
+    catch(err){
+        res.status(500).json({msg:err})
+    }
 }
-const getTask=(req,res)=>{
-    res.json({id:req.params.id})
+const getTask = async (req,res)=>{
+    try{
+        const {id:taskID}=req.params
+        const task=await Task.findOne({_id:taskID})
+        if (!task){
+            return res.status(404).json({msg:`No such task Exist`})
+        }
+        res.status(201).json({task})
+    }
+    catch(err){
+        res.status(500).json({msg:err})
+    }
 }
-const updateTask=(req,res)=>{
-    res.send('Update Task')
+const updateTask=async(req,res)=>{
+    try {
+        const {id:taskID}=req.params
+        const task=await Task.findOneAndUpdate({_id:taskID},req.body,{
+            new:true,
+            runValidators:true,
+        })
+        if (!task){
+            return res.status(404).json({msg:`No such task Exist`})
+        }
+        res.status(200).json({task})
+    } catch (err) {
+        res.status(500).json({msg:err})
+    }   
 }
-const deleteTask=(req,res)=>{
-    res.send('Deleted The Task')
+const deleteTask=async (req,res)=>{
+    try {
+        const {id:taskID}=req.params
+        const task=await Task.findOneAndDelete({_id:taskID})
+        if (!task){
+            return res.status(404).json({msg:`No such task Exist to delete`})
+        }
+        res.status(200).send()
+    } catch (error) {
+        res.status(500).json({msg:err})
+    }
 }
 
 
